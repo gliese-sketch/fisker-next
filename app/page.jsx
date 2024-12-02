17c747a722e5b2e5e42107a9f5acfd7715ac3e4b
gliese-sketch <173663566+gliese-sketch@users.noreply.github.com>
2024-12-02 07:43:29 +0000

    Add Header.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,10 @@
+import Header from "@/components/Header";
+
 function HomePage() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-8xl font-bold text-center">Fisker Next</h1>
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
 export default HomePage;
