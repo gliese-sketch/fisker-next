e189b27e4a0e05c739e278a52d208c303fdd0c8a
gliese-sketch <173663566+gliese-sketch@users.noreply.github.com>
2024-12-02 07:16:32 +0000

    Bootstrap Next App

---
+++ app/page.jsx
@@ -0,0 +1,8 @@
+function HomePage() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-8xl font-bold text-center">Fisker Next</h1>
+    </div>
+  );
+}
+export default HomePage;
