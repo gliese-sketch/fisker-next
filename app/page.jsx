3ecb819699900c0fd779a86ba740d3e4cba5c30e
gliese-sketch <173663566+gliese-sketch@users.noreply.github.com>
2024-12-02 07:58:38 +0000

    Layout in HomePage

--- app/page.jsx
+++ app/page.jsx
@@ -4,6 +4,14 @@ function HomePage() {
   return (
     <>
       <Header />
+      <main className="flex flex-col sm:flex-row justify-between md:gap-8">
+        <section className="w-full md:w-3/4 bg-blue-400">
+          Preview Component
+        </section>
+        <aside className="w-full md:w-1/4 bg-red-300">
+          Configurator Component
+        </aside>
+      </main>
     </>
   );
 }
