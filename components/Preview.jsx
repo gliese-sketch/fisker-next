932a75bbf54ff8d0c3ef818e3fb1e8adc927206e
gliese-sketch <173663566+gliese-sketch@users.noreply.github.com>
2024-12-04 07:51:05 +0000

    Preview component

--- components/Preview.jsx
+++ components/Preview.jsx
@@ -1,11 +1,21 @@
 import Image from "next/image";
+import configs from "@/app/assets/configs.json";
 
-function Preview() {
+// @desc Preview component
+// @param {string} color: Sun Soaked/Sea Grass/Black Pearl/Red Planet/Stealth Green/Blue Planet
+// @param {string} interior: dark/white
+// @param {string} wheel: AeroStealth/SlipStream Black/Vortex
+function Preview({
+  color = "Black Pearl",
+  interior = "white",
+  wheel = "Vortex",
+}) {
+  console.log(`images/interior/${interior}.jpeg`);
   return (
     <>
       {/* Exterior Image */}
       <Image
-        src={"/images/aerostealth.jpg"}
+        src={configs[color].wheels[wheel]}
         width="0"
         height="0"
         sizes="100vw"
@@ -14,13 +24,15 @@ function Preview() {
       />
       {/* Description */}
       <div className="py-3">
-        <h3 className="font-bold text-xl text-blue-950">Metallic Gloss</h3>
-        <p>A mysterious, seductive black with a metallic blue shimmer.</p>
+        <h3 className="font-bold text-xl text-blue-950">
+          {configs[color].finish}
+        </h3>
+        <p>{configs[color].description}</p>
       </div>
 
       {/* Interior Image */}
       <Image
-        src={"/images/dark.jpeg"}
+        src={`/images/interior/${interior}.jpeg`}
         width="0"
         height="0"
         sizes="100vw"
